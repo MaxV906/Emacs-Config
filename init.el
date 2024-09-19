4c3c3b5a230374efce03f8238a1d9859b22e0470
MaxV <maxv906@proton.me>
2024-09-19 13:30:59 +0200

    Emacs Config

---
+++ init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66" default))
+ '(package-selected-packages
+   '(evil-numbers projectile mu4easy mu4e multi-vterm langtool magit beacon xwidgets-reuse which-key vterm use-package rainbow-delimiters org-bullets lsp-ui lsp-treemacs lsp-ivy evil-collection doom-themes doom-modeline counsel command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
