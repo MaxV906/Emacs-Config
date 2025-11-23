b78829e7f28364df763d739ce035b4c3017f1afe
MaxV <maxv906@proton.me>
2025-11-23 20:36:54 +0100

    Added company and cargo mode

--- init.el
+++ init.el
@@ -8,9 +8,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66" default))
+   '("f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66"
+     default))
  '(package-selected-packages
-   '(evil-numbers projectile mu4easy mu4e multi-vterm langtool magit beacon xwidgets-reuse which-key vterm use-package rainbow-delimiters org-bullets lsp-ui lsp-treemacs lsp-ivy evil-collection doom-themes doom-modeline counsel command-log-mode)))
+   '(beacon cargo-mode command-log-mode company counsel doom-modeline
+            doom-themes evil-collection evil-numbers flycheck
+            flycheck-rust lsp-ivy lsp-treemacs lsp-ui magit
+            multi-vterm ob-rust org-bullets projectile
+            rainbow-delimiters rustic)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
